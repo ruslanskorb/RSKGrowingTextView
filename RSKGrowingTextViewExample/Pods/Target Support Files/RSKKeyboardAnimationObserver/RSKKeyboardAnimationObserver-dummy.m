fc0897e4cef13bb287b5284cc965d4af7777a035
Ruslan Skorb <ruslan@rsk-lab.com>
2023-09-09 14:03:38 +0300

    [RSKGrowingTextViewExample] Remove archived `RSKKeyboardAnimationObserver` pod + Bump `IPHONEOS_DEPLOYMENT_TARGET` (12.0) + Add support for Xcode 14.3.1 + Migrate to Swift 5.

--- RSKGrowingTextViewExample/Pods/Target Support Files/RSKKeyboardAnimationObserver/RSKKeyboardAnimationObserver-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RSKKeyboardAnimationObserver : NSObject
-@end
-@implementation PodsDummy_RSKKeyboardAnimationObserver
-@end
--- RSKGrowingTextViewExample/RSKGrowingTextViewExample/ViewController.swift
+++ RSKGrowingTextViewExample/RSKGrowingTextViewExample/ViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Ruslan Skorb. All rights reserved.
 //
 
-import RSKKeyboardAnimationObserver
+import UIKit
 
 class ViewController: UIViewController {
     
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
     
     private func adjustContent(for keyboardRect: CGRect) {
         let keyboardHeight = keyboardRect.height
-        self.bottomLayoutGuideTopAndGrowingTextViewBottomVeticalSpaceConstraint.constant = self.isVisibleKeyboard ? keyboardHeight - self.bottomLayoutGuide.length : 0.0
+        self.bottomLayoutGuideTopAndGrowingTextViewBottomVeticalSpaceConstraint.constant = self.isVisibleKeyboard ? keyboardHeight - self.view.safeAreaInsets.bottom : 0.0
         self.view.layoutIfNeeded()
     }
     
@@ -44,23 +44,58 @@ class ViewController: UIViewController {
         self.growingTextView.resignFirstResponder()
     }
     
-    private func registerForKeyboardNotifications() {
-        self.rsk_subscribeKeyboardWith(beforeWillShowOrHideAnimation: nil,
-            willShowOrHideAnimation: { [unowned self] (keyboardRectEnd, duration, isShowing) -> Void in
-                self.isVisibleKeyboard = isShowing
-                self.adjustContent(for: keyboardRectEnd)
-            }, onComplete: { (finished, isShown) -> Void in
-                self.isVisibleKeyboard = isShown
+    @objc private func handle(keyboardNotification: Notification) {
+        guard let userInfo = keyboardNotification.userInfo, let keyboardFrameEnd = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        let keyboardRect = self.view.convert(keyboardFrameEnd.cgRectValue, from: nil)
+        switch keyboardNotification.name {
+        case UIResponder.keyboardDidHideNotification, UIResponder.keyboardWillHideNotification:
+            self.isVisibleKeyboard = false
+        case UIResponder.keyboardDidShowNotification, UIResponder.keyboardWillShowNotification:
+            self.isVisibleKeyboard = true
+        case UIResponder.keyboardWillChangeFrameNotification:
+            break
+        default:
+            return
+        }
+        let duration = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0.0
+        var options: UIView.AnimationOptions = [.beginFromCurrentState]
+        let keyboardAnimationCurve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber
+        if let keyboardAnimationCurve = keyboardAnimationCurve?.intValue,
+           let animationCurve = UIView.AnimationCurve(rawValue: keyboardAnimationCurve) {
+            switch animationCurve {
+            case .easeInOut:
+                options.insert(.curveEaseInOut)
+            case .easeIn:
+                options.insert(.curveEaseIn)
+            case .easeOut:
+                options.insert(.curveEaseOut)
+            case .linear:
+                options.insert(.curveLinear)
+            @unknown default:
+                break
             }
-        )
-        
-        self.rsk_subscribeKeyboard(willChangeFrameAnimation: { [unowned self] (keyboardRectEnd, duration) -> Void in
-            self.adjustContent(for: keyboardRectEnd)
-        }, onComplete: nil)
+        }
+        UIView.animate(withDuration: duration, delay: 0.0, options: options, animations: {
+            self.adjustContent(for: keyboardRect)
+        })
+    }
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handle(keyboardNotification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handle(keyboardNotification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handle(keyboardNotification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handle(keyboardNotification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handle(keyboardNotification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
     }
     
     private func unregisterForKeyboardNotifications() {
-        self.rsk_unsubscribeKeyboard()
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
     }
 }
 
